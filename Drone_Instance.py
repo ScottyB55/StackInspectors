86d2c2b1048a88ab13fdfb0626631ecd417365b8
Anupam Mijar <aamijar230@gmail.com>
2023-03-08 16:30:21 -0500

    add keyboard input and gps print

--- Drone_Instance.py
+++ Drone_Instance.py
@@ -27,6 +27,24 @@ drone = Guided_Drone(connection_string)
 drone.takeoff(target_altitude=3, altitude_reach_threshold=0.95)
 
 
+target_roll = 0
+target_pitch = 0
+
+def on_press(key):
+    global target_roll
+    global target_pitch
+    if key.char == "w":
+        print("w pressed")
+        target_pitch = -10
+    elif key.char == "a":
+        print("a pressed")
+        target_roll = -10
+    elif key.char == "s":
+        print("s pressed")
+        target_pitch = 10
+    elif key.char == "d":
+        print("d pressed")
+        target_roll = 10
 
 # save initial yaw to use as target
 #target_yaw = math.degrees(vehicle.attitude.yaw)
@@ -35,11 +53,15 @@ iteration_time = .1
 run = 1
 
 try:
-    while run:
-        # hover_thrust (0-1 where 0.5 is no vertical velocity)
-        drone.set_attitude(target_roll=1, target_pitch=0, target_yaw=0, hover_thrust=0.5)
-        time.sleep(iteration_time)
-
+    # Collect events until stopped
+    from pynput import keyboard
+    with keyboard.Listener(on_press=on_press) as listener:
+        while run:
+            # hover_thrust (0-1 where 0.5 is no vertical velocity)
+            drone.set_attitude(target_roll=target_roll, target_pitch=target_pitch, target_yaw=0, hover_thrust=0.5)
+            print(drone.vehicle.location.global_relative_frame)
+            time.sleep(iteration_time)
+            
 except KeyboardInterrupt:
     print('exiting')
 
