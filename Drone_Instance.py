01bc90188706cc48f7a55af34ff056a5d1467b41
Scott Burnett <srburnett111@gmail.com>
2023-03-29 18:19:01 -0400

    Mouse controlled drone :)

--- Drone_Instance.py
+++ Drone_Instance.py
@@ -6,6 +6,9 @@ from Guided_Drone_Class import Guided_Drone
 import math
 import time
 
+# import pygetwindow as gw
+import pyautogui
+
 # The code to get the drone connected
 
 # Set up option parsing to get connection string and mission plan file
@@ -46,6 +49,16 @@ def on_press(key):
         print("d pressed")
         target_roll = 10
 
+def mouse_relative_position_from_center_normalized():
+    screen_width, screen_height = pyautogui.size()
+    center_x, center_y = screen_width / 2, screen_height / 2
+    mouse_x, mouse_y = pyautogui.position()
+
+    relative_x = (mouse_x - center_x) / (center_x)
+    relative_y = (mouse_y - center_y) / (center_y)
+
+    return relative_x, relative_y
+
 # save initial yaw to use as target
 #target_yaw = math.degrees(vehicle.attitude.yaw)
 # Set the period of the loop in seconds
@@ -54,12 +67,19 @@ run = 1
 
 try:
     # Collect events until stopped
+    target_roll = 1
+    target_pitch = 1
     from pynput import keyboard
     with keyboard.Listener(on_press=on_press) as listener:
         while run:
+            mouse_x, mouse_y = mouse_relative_position_from_center_normalized()
+            print(f"Mouse relative position from the center of the screen: x={mouse_x:.2f}, y={mouse_y:.2f} pixels")
+
             # hover_thrust (0-1 where 0.5 is no vertical velocity)
-            drone.set_attitude(target_roll=target_roll, target_pitch=target_pitch, target_yaw=0, hover_thrust=0.5)
+            drone.set_attitude(target_roll=mouse_x, target_pitch=mouse_y, target_yaw=0, hover_thrust=0.5)
+
             print(drone.vehicle.location.global_relative_frame)
+
             time.sleep(iteration_time)
             
 except KeyboardInterrupt:
