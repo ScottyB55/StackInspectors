444d952abccbecbda072a46d77b0c5fc79d3a533
Scott Burnett <srburnett111@gmail.com>
2023-03-31 15:44:28 -0400

    barely changed anything

--- Guided_Drone_Class.py
+++ Guided_Drone_Class.py
@@ -45,6 +45,27 @@ def rc_listener(self, name, message):
     rcin_4_center = (message.chan4_raw < 1550 and message.chan4_raw > 1450)
     # print(f"Message Chan4_Raw: {message.chan4_raw}")
 
+def get_location_metres(original_location, dNorth, dEast, altitude):
+    """
+    Returns a LocationGlobal object containing the latitude/longitude `dNorth` and `dEast` metres from the 
+    specified `original_location`.
+
+    The function is useful when you want to move the vehicle around specifying locations relative to 
+    the current vehicle position.
+    The algorithm is relatively accurate over small distances (10m within 1km) except close to the poles.
+    For more information see:
+    http://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
+    """
+    earth_radius=6378137.0 #Radius of "spherical" earth
+    #Coordinate offsets in radians
+    dLat = dNorth/earth_radius
+    dLon = dEast/(earth_radius*math.cos(math.pi*original_location.lat/180))
+
+    #New position in decimal degrees
+    newlat = original_location.lat + (dLat * 180/math.pi)
+    newlon = original_location.lon + (dLon * 180/math.pi)
+    return LocationGlobalRelative(newlat, newlon, altitude)
+
 class Guided_Drone:
     """
     A class for controlling a drone in guided mode using DroneKit.
