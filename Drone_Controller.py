eea6731d172e6771ed918524627007652415a303
cbeiler <cbeiler@ncsu.edu>
2023-04-18 19:43:54 -0400

    added angle ignore for landing gear

--- Drone_Controller.py
+++ Drone_Controller.py
@@ -128,7 +128,7 @@ class Drone_Controller:
         closest_point = None
 
         for lidar_reading in lidar_readings:
-            if min_distance is None or lidar_reading.total_relative_distance_m < min_distance:
+            if min_distance is None or lidar_reading.total_relative_distance_m < min_distance or (lidar_reading.angle>50 and lidar_reading.angle<60) or (lidar_reading.angle>135 and lidar_reading.angle<145) or (lidar_reading.angle>215 and lidar_reading.angle<225) or (lidar_reading.angle>300 and lidar_reading.angle<310):
                 min_distance = lidar_reading.total_relative_distance_m
                 closest_point = lidar_reading
         
